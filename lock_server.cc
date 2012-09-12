7670dd1471095b410737bfd234626910508bf50b
Liangzhou Yi <liangzhou.yi@nyu.edu>
2012-09-12 02:03:16 -0400

    Add logical to implement lock server

--- lock_server.cc
+++ lock_server.cc
@@ -20,4 +20,48 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
   return ret;
 }
 
+// the reply value r doesn't matter
+lock_protocol::status
+lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  lock_protocol::status ret = lock_protocol::OK;
+  
+  // to do No.1
+  if(lockToStatus.find(lid) == lockToStatus.end()) {
+    // lockToStatus.insert(lid, LOCKED);
+    
+  } else {
+    // lock exist
+    if(LOCKED == lockToStatus[lid]) {
+      // block current thread
+      // and wait for a signal
+    } else {
+      lockToStatus[lid] = LOCKED;
+      // this will block other acquire requests
+    }
+  }
+  
+  // the reply vlaue r doesn't matter
+  r = nacquire;
+
+  // since no other place will make ret false, it will always return OK
+  // but be aware of the timeout, check that later
+  return ret;
+}
+
+lock_protocol::status
+lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  lock_protocol::status ret = lock_protocol::OK;
+  // printf("release request from clt %d\n", clt);
+  
+  // VERIFY(lockToStatus[lid] == LOCKED)
+  lockToStatus[lid] = FREE;
+
+  // notify any threads are waiting for the lock
+
+  r = nacquire;
+  return ret;
+}
+
 
--- lock_server.h
+++ lock_server.h
@@ -13,11 +13,21 @@ class lock_server {
 
  protected:
   int nacquire;
+  
+  // each lock is identified by an integer of type lock_protocol::lockit_t
+  // use std::map to hold the table of lock states
+  enum LockStatus { FREE, LOCKED };
+
+  typedef std::map<lock_protocol::lockid_t, LockStatus> LockToStatus;
+  LockToStatus lockToStatus;
+
 
  public:
   lock_server();
   ~lock_server() {};
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
 };
 
 #endif 
